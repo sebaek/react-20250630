4903bf1499ef3582c4343705d166abcb16a76e5c
sebaek <sebaek@gmail.com>
2025-07-01 14:43:19 +0900

    react bootstrap

--- src/App16.jsx
+++ src/App16.jsx
@@ -17,6 +17,70 @@ function App16() {
       <hr />
       <Alert variant="danger">위험 경고</Alert>
       <Alert variant="info">정보 알림</Alert>
+      {/*  연습 : Alert 만들어보기*/}
+      <hr />
+      {/*  연습 : Table component 사용해서 아래와 같은 table 만들어보기  */}
+      {/*table.table.table-striped.table-bordered.table-hover>thead>tr>th*3>lorem1^^^tbody>tr*3>td*3>lorem1*/}
+      <table className="table table-striped table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Lorem.</th>
+            <th>Doloribus!</th>
+            <th>Nostrum?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Lorem.</td>
+            <td>Quos.</td>
+            <td>Eligendi.</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Molestias.</td>
+            <td>Sequi.</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Commodi!</td>
+            <td>Officiis.</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Lorem.</td>
+            <td>Autem!</td>
+            <td>Sequi.</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Delectus?</td>
+            <td>Ipsum!</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Vero.</td>
+            <td>Non!</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Lorem.</td>
+            <td>Aliquam!</td>
+            <td>Accusamus?</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Laudantium!</td>
+            <td>Perferendis.</td>
+          </tr>
+          <tr>
+            <td>Lorem.</td>
+            <td>Doloribus?</td>
+            <td>Culpa?</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
