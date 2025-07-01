142d7db6594077d82811e51ab373c8b66b2f034d
sebaek <sebaek@gmail.com>
2025-07-01 10:49:36 +0900

    props, 분해할당, 기본값

--- src/App6.jsx
+++ src/App6.jsx
@@ -20,6 +20,18 @@ function MyComp3({ address }) {
   return null;
 }
 
+function MyComp4({ city }) {
+  console.log("도시", city);
+  return null;
+}
+
+function MyComp5({ city, country, address = "기본 주소" }) {
+  console.log("도시5", city);
+  console.log("나라5", country);
+  console.log("주소5", address);
+  return null;
+}
+
 function App6() {
   return (
     <>
@@ -34,6 +46,15 @@ function App6() {
       <MyComp4 city="서울" />
       <MyComp4 city="워싱턴" />
       {/*  MyComp4 함수 작성 w/ 분해 할당으로 props 받기 -> 로그 출력*/}
+      <MyComp5 city="인천" country="한국" address="강남" />
+      <MyComp5 city="텍사스" country="미국" address="라스베가스" />
+      <MyComp5 city="오사카" country="일본" />
+      {/*  연습 : MyComp6 이 받은 props 들 출력
+            location의 기본값은 "기본위치"
+      */}
+      <MyComp6 score="A" className="중앙" location="신촌" />
+      <MyComp6 score="B" className="센터" location="강남" />
+      <MyComp6 score="C" className="기린" />
     </>
   );
 }
