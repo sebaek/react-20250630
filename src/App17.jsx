13de78fe78bee2502c7a7ca48a3f04ad2964f123
sebaek <sebaek@gmail.com>
2025-07-01 15:26:17 +0900

    react icons

--- src/App17.jsx
+++ src/App17.jsx
@@ -1,10 +1,16 @@
 import { FaDog } from "react-icons/fa6";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { HiBookmark as Bookmark1 } from "react-icons/hi";
+import { HiBookmark as Bookmark2 } from "react-icons/hi2";
 
 function App17() {
   return (
     <div>
       <FaDog />
       {/*  연습 아이콘 사용해 보기 */}
+      <BiLogoFacebookCircle />
+      <Bookmark1 />
+      <Bookmark2 />
     </div>
   );
 }
