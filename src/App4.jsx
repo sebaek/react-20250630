86520aab630b889ba8dac3aca3191b28205e3580
sebaek <sebaek@gmail.com>
2025-07-01 10:21:36 +0900

    컴포넌트의 가장 상위 요소는 하나

---
+++ src/App4.jsx
@@ -0,0 +1,68 @@
+function MyComp1() {
+  return <h1>Hello React</h1>;
+}
+
+// 이름은 UpperCamelCase
+// return 에 그려야할 html 코드 작성
+/// 가장의 상위 요소가 하나인 것만 그려야 함
+function MyComp2() {
+  return <h2>Hello Component</h2>;
+}
+
+function MyComp3() {
+  return (
+    <h1>
+      <span>안녕</span>
+      <span>컴포넌트</span>
+      <button>
+        <span>버튼 문구</span>
+      </button>
+    </h1>
+  );
+}
+
+function MyComp4() {
+  return (
+    <div>
+      <div>hello</div>
+      <div>comp4</div>
+    </div>
+  );
+}
+
+function MyComp5() {
+  // fragment : 가장 바깥 요소가 필요 없을 때 <></> 사용
+
+  return (
+    <>
+      <div>hello</div>
+      <div>comp5</div>
+    </>
+  );
+}
+
+function App4() {
+  return (
+    <>
+      <MyComp1 />
+      <MyComp2 />
+      <MyComp3 />
+      <MyComp4 />
+      <MyComp5 />
+      <MyComp6 />
+      {/*
+      <div>
+        <h1>제목</h1>
+        <p>lorem3</p>
+      </div>
+      */}
+      <MyComp7 />
+      {/*
+      <h2>두번째 제목</h2>
+      <p>lorem3</p>
+      */}
+    </>
+  );
+}
+
+export default App4;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App3 from "./App3.jsx";
+import App4 from "./App4.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App3 />
+    <App4 />
   </StrictMode>,
 );
