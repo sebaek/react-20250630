56d5f5aab74aee99590a8d0ca3a6a35af149c968
sebaek <sebaek@gmail.com>
2025-07-01 09:37:34 +0900

    리액트 코드 연습

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import App1 from "./App1.jsx";
+import App2 from "./App2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
