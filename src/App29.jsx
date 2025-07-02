5e8b737bf977a5b5c10deca28cdc78ee4b0b48cb
sebaek <sebaek@gmail.com>
2025-07-02 15:36:45 +0900

    상태가 배열일 때 복사한 새 배열로 상태 변경해야 함

--- src/App29.jsx
+++ src/App29.jsx
@@ -113,6 +113,7 @@ function MyComp5() {
       <button onClick={handleEagleButtonClick}>독수리 추가</button>
       <button onClick={handleBeeButtonClick}>벌 추가</button>
       <button onClick={() => setList([...list, "사자"])}>사자 추가</button>
+      <button>마지막 요소 지우기</button>
       <ul>
         {list.map((item) => (
           <li>{item}</li>
