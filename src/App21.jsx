b8186f385dd133a2a8872f50c2532e9e7e0031d3
sebaek <sebaek@gmail.com>
2025-07-02 10:24:18 +0900

    이벤트 버블링

---
+++ src/App21.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App21(props) {
+  function handleButtonClick(e) {
+    // e : event 객체
+    console.log(e);
+
+    // stopPropagation() : 이벤트 전달 막기
+    e.stopPropagation();
+    console.log("버튼 클릭");
+  }
+
+  return (
+    <div className="p-5 bg-info" onClick={() => console.log("div 클릭")}>
+      {/* 연습:  p 클릭 이벤트가 div까지 전파되지 않도록 코드 수정*/}
+      <p className="bg-danger" onClick={() => console.log("para 클릭")}>
+        Lorem ipsum dolor.
+      </p>
+
+      <button onClick={handleButtonClick}>버튼</button>
+    </div>
+  );
+}
+
+export default App21;
