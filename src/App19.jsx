13d9cbe4340008d6bf15fc89e936e4d100b6d3bf
sebaek <sebaek@gmail.com>
2025-07-02 09:37:08 +0900

    이벤트 props

--- src/App19.jsx
+++ src/App19.jsx
@@ -20,14 +20,30 @@ function App19(props) {
     console.log("버튼 클릭됨");
   };
 
+  // on... 에 들어가는 함수 이름은 handle...
+  function handleButton1Click() {
+    console.log("버튼1 클릭됨");
+  }
+
+  function handleButton2DoubleClick() {
+    console.log("두번째 버튼 더블 클릭됨");
+  }
+
   return (
     <div>
       {/*연습 : 버튼1 클릭하면 콘솔에 메세지 출력*/}
       {/*연습 : 버튼2 더블클릭하면 콘솔에 메세지 출력*/}
       {/*연습 : 버튼3 마우스커서가 들어가면 콘솔에 메세지 출력*/}
-      <button>버튼1</button>
-      <button>버튼2</button>
-      <button>버튼3</button>
+      <button onClick={handleButton1Click}>버튼1</button>
+      <button onDoubleClick={handleButton2DoubleClick}>버튼2</button>
+      <button
+        onMouseEnter={function () {
+          console.log("버튼3에 마우스 들어감");
+        }}
+        onMouseLeave={() => console.log("버튼3에서 마우스 나감")}
+      >
+        버튼3
+      </button>
 
       <hr />
       <button onClick={action4} style={{ padding: "10px" }}>
