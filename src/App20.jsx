a5a7b30b1d924647cac048c3a51c037a5499c733
sebaek <sebaek@gmail.com>
2025-07-02 09:47:07 +0900

    이벤트 props

---
+++ src/App20.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function AwesomeButton({ children, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        padding: "10px",
+        backgroundColor: "black",
+        color: "white",
+        fontSize: "2rem",
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+function App20(props) {
+  return (
+    <div>
+      {/*연습 : AwesomeHeader 를 만들고 마우스가 더블클릭하면 콘솔에 메시지 출력*/}
+      {/* <h1 style={적절한 스타일 주기}></h1>*/}
+
+      <button onClick={() => console.log("보통의 버튼 클릭")}>
+        브라우저 컴포넌트 버튼
+      </button>
+      <AwesomeButton onClick={() => console.log("멋진버튼1 클릭")}>
+        멋진버튼1
+      </AwesomeButton>
+      <Button
+        variant="outline-danger"
+        onClick={() => console.log("부트스트랩 버튼 클릭")}
+      >
+        부트스트랩 버튼
+      </Button>
+    </div>
+  );
+}
+
+export default App20;
