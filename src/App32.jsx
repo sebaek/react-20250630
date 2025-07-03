19d45d943fe118d30e99e590a397e5a20ea4e145
sebaek <sebaek@gmail.com>
2025-07-03 09:29:00 +0900

    객체내의 또 다른 객체가 있다면 깊은 복사를 해야함

--- src/App32.jsx
+++ src/App32.jsx
@@ -74,7 +74,76 @@ function MyComp1() {
 }
 
 function MyComp2() {
-  return null;
+  const [person, setPerson] = useState({
+    name: "",
+    address: { city: "", country: "" },
+  });
+  const [history, setHistory] = useState([]);
+
+  function handleNameChange(e) {
+    const nextPerson = { ...person }; // shallow copy (하지 말 것)
+    nextPerson.name = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleCityChange(e) {
+    const nextPerson = { ...person }; // shallow copy (하지 말 것)
+    nextPerson.address.city = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleCountryChange(e) {
+    const nextPerson = { ...person }; // shallow copy (하지 말 것)
+    nextPerson.address.country = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleSaveClick(e) {
+    const nextHistory = [...history];
+    nextHistory.push(person);
+    setHistory(nextHistory);
+  }
+
+  return (
+    <div>
+      {/*div.d-flex>div.flex-1*2*/}
+      <div className="d-flex">
+        <div className="flex-1">
+          <div>
+            <input
+              type="text"
+              value={person.name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={person.address.city}
+              onChange={handleCityChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={person.address.country}
+              onChange={handleCountryChange}
+            />
+          </div>
+          <div>
+            <button onClick={handleSaveClick}>변경</button>
+          </div>
+        </div>
+        <div className="flex-1">
+          <h3>변경 이력</h3>
+          {history.map((item) => (
+            <div className="border my-1">
+              <div>이름 : {item.name}</div>
+              <div>도시 : {item.address.city}</div>
+              <div>국가 : {item.address.country}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function App32(props) {
