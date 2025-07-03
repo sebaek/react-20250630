4b5f3b66e5f293a9fd1cd6ec27e26e3192315f72
sebaek <sebaek@gmail.com>
2025-07-03 12:35:11 +0900

    component lifecycle

--- src/App36.jsx
+++ src/App36.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
+function ChildComp11() {
+  const [count, setCount] = useState(1);
+
+  return (
+    <div className="border bg-info">
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>증가</button>
+    </div>
+  );
+}
+
 function MyComp1() {
   const [checked, setChecked] = useState(false);
+  const [checked2, setChecked2] = useState(false);
 
-  console.log(checked);
   return (
     <div>
       <Form.Check
@@ -12,9 +23,25 @@ function MyComp1() {
         checked={checked}
         onChange={() => setChecked(!checked)}
       />
+      {checked && <ChildComp11 />}
+
+      <hr />
+
+      <Form.Check
+        type="switch"
+        checked={checked2}
+        onChange={() => setChecked2(!checked2)}
+      />
+      {checked2 && <ChildComp11 />}
     </div>
   );
 }
+// component의 lifecycle :
+// initial render : 컴포넌트 트리에 붙을 때
+// re-render : 상태가 변경될 때
+// 트리에서 떨어짐
+
+// useEffect : component의 lifecycle에 맞춰 실행시킬 수 있는 메소드를 가짐
 
 function App36(props) {
   return (
