e7b9f7a997e4f20b431701d9dd712132602afd3d
sebaek <sebaek@gmail.com>
2025-07-03 15:46:20 +0900

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App36 from "./App36.jsx";
+import App37 from "./App37.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App36 />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<App37 />);
