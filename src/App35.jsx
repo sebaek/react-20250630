9a4b429ebb67767e883d1db4b2100daa1fdb600d
sebaek <sebaek@gmail.com>
2025-07-03 11:24:13 +0900

    prop drilling

---
+++ src/App35.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+function ChildComp13({ text }) {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+function ChildComp12({ text }) {
+  return (
+    <div>
+      <ChildComp13 text={text} />
+    </div>
+  );
+}
+
+function ChildComp11({ text }) {
+  return (
+    <div>
+      <ChildComp12 text={text} />
+    </div>
+  );
+}
+
+function MyComp1() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <ChildComp11 text={text} />
+    </div>
+  );
+}
+
+function ChildComp23() {
+  return (
+    <div>
+      <h3>목록</h3>
+      <ul>{/*  목록 출력 */}</ul>
+    </div>
+  );
+}
+
+function ChildComp22() {
+  return (
+    <div>
+      <ChildComp23 />
+    </div>
+  );
+}
+
+function ChildComp21() {
+  return (
+    <div>
+      <ChildComp22 />
+    </div>
+  );
+}
+
+function MyComp2() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  function handleAddClick() {
+    setList([...list, text]);
+  }
+  return (
+    <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={handleAddClick}>추가</button>
+      <ChildComp21 />
+    </div>
+  );
+}
+
+function App35(props) {
+  return (
+    <div>
+      <MyComp2 />
+      <hr />
+      {/*prop drilling*/}
+      <MyComp1 />
+    </div>
+  );
+}
+
+export default App35;
