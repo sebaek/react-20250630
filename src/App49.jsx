542bed7729b275aee9a5e4ceece39c3bf1e73ef5
sebaek <sebaek@gmail.com>
2025-07-04 14:23:48 +0900

    layout

---
+++ src/App49.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 연습 : 아래 경로에 같은 sidebar가 존재하도록 코드 작성
+
+// /react/install
+// /react/game
+// /react/state
+
+function App49(props) {
+  return <div></div>;
+}
+
+export default App49;
