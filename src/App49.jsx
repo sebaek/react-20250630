ac3bd2c11438f56f42243fb01129e8bdeb44987a
sebaek <sebaek@gmail.com>
2025-07-04 15:31:33 +0900

    Link 컴포넌트 : 경로 이동 시 전체 페이지 로딩 방지

--- src/App49.jsx
+++ src/App49.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router";
 
 // 연습 : 아래 경로에 같은 sidebar가 존재하도록 코드 작성
 // 연습 : 경로 이동 시 모든 컴포넌트가 다시 마운트 되지 않도록 코드 수정
@@ -9,17 +9,26 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 // /react/state
 
 function ReactComponent() {
+  useEffect(() => {
+    //
+    console.log("마운트 때 실행 코드");
+
+    return () => {
+      console.log("언마운트 때 실행되는 코드");
+    };
+  }, []);
+
   return (
     <div className="d-flex gap-3">
       <div className="w-25 bg-body-tertiary p-1">
         <div>
-          <a href="/react/install">인스톨</a>
+          <Link to="/react/install">인스톨</Link>
         </div>
         <div>
-          <a href="/react/game">게임</a>
+          <Link to="/react/game">게임</Link>
         </div>
         <div>
-          <a href="/react/state">상태</a>
+          <Link to="/react/state">상태</Link>
         </div>
       </div>
       <div className="flex-grow-1 bg-primary-subtle">
