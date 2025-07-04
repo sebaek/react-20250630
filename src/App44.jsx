8c3e4f445f082e17cd6318f4d78e174c1bbae989
sebaek <sebaek@gmail.com>
2025-07-04 10:48:38 +0900

    nested routes

---
+++ src/App44.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 연습 : 아래 경로에 따라 해당 컴포넌트 출력하기
+
+// /learn/react : <h3>리액트 배우기</h3>
+// /learn/spring : <h3>스프링 배우기</h3>
+// /learn/java/collection : <h3>자바 콜렉션 배우기</h3>
+
+function App44(props) {
+  return <div></div>;
+}
+
+export default App44;
