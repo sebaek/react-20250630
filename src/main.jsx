6874776e009528cb769fa137839752bd3d8f5069
sebaek <sebaek@gmail.com>
2025-07-04 10:19:03 +0900

    요청 경로에 따른 컴포넌트 구성

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App39 from "./App39.jsx";
+import App40 from "./App40.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App39 />
+    <App40 />
   </StrictMode>,
 );
