85b687bf108fb7907b4dd014cfde27d816eec0f9
sebaek <sebaek@gmail.com>
2025-07-04 15:18:48 +0900

    layout

--- src/App49.jsx
+++ src/App49.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 
 // 연습 : 아래 경로에 같은 sidebar가 존재하도록 코드 작성
 
@@ -6,8 +7,62 @@ import React from "react";
 // /react/game
 // /react/state
 
+function ReactComponent() {
+  return (
+    <div className="d-flex gap-3 p-5">
+      <div className="w-25 bg-body-tertiary">
+        <div>
+          <a href="/react/install">인스톨</a>
+        </div>
+        <div>
+          <a href="/react/game">게임</a>
+        </div>
+        <div>
+          <a href="/react/state">상태</a>
+        </div>
+      </div>
+      <div className="flex-1 bg-primary-subtle">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 function App49(props) {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="react" element={<ReactComponent />}>
+            <Route
+              path="install"
+              element={
+                <div>
+                  <h3>설치 방법</h3>
+                </div>
+              }
+            />
+            <Route
+              path="game"
+              element={
+                <div>
+                  <h3>게임 만들기</h3>
+                </div>
+              }
+            />
+            <Route
+              path="state"
+              element={
+                <div>
+                  <h3>상태란 무엇인가?</h3>
+                </div>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App49;
