db2e4241c20a138c40dc09f62edbb1e25d6edc75
sebaek <sebaek@gmail.com>
2025-07-04 09:28:44 +0900

    strict mode, pure component

---
+++ src/App38.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { FormCheck } from "react-bootstrap";
+
+function MyComp1({ value }) {
+  return <div className="border">value * 2 : {value * 2}</div>;
+}
+
+let someValue = 0;
+function MyComp2() {
+  someValue = someValue + 1;
+  return <div className="border bg-light">someValue : {someValue}</div>;
+}
+
+function MyComp3() {
+  useEffect(() => {
+    // 마운트 될 때 실행 코드
+    console.log("comp3 마운트됨");
+
+    return () => {
+      // 언마운트 될 때 실행 코드
+      console.log("comp3 언마운트됨");
+    };
+  }, []);
+  return (
+    <div>
+      <h3>mycomp 3</h3>
+    </div>
+  );
+}
+
+function Child41() {
+  useEffect(() => {
+    console.log("child41 mounted");
+    return () => {
+      console.log("child4 unmounted");
+    };
+  }, []);
+  return (
+    <div>
+      <h3>child41 component</h3>
+    </div>
+  );
+}
+function MyComp4() {
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <FormCheck type="switch" checked={show} onClick={() => setShow(!show)} />
+      {show && <Child41 />}
+    </div>
+  );
+}
+
+function App38(props) {
+  // strict mode : 모든 컴포넌트를 마운트,언마운트,마운트
+  return (
+    <div>
+      <MyComp4 />
+      <hr />
+      <MyComp3 />
+      <hr />
+      <MyComp2 />
+      <MyComp2 />
+      <MyComp2 />
+      <hr />
+      <MyComp1 value={1} />
+      <MyComp1 value={2} />
+    </div>
+  );
+}
+
+export default App38;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App37 from "./App37.jsx";
+import App38 from "./App38.jsx";
 
-createRoot(document.getElementById("root")).render(<App37 />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App38 />
+  </StrictMode>,
+);
