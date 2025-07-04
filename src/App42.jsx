05995eb50b03cca525a1d97f56876e1f82f9c4bf
sebaek <sebaek@gmail.com>
2025-07-04 10:33:10 +0900

    react router

---
+++ src/App42.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 연습 : react router 사용해서 아래 경로에 따른 컴포넌트 다르게 보이기
+// localhost:5173/learn : <h3>react 배우기</h3>
+// localhost:5173/tutorial : <h3>react 공부하기</h3>
+// localhost:5173/install : <h3>react 설치하기</h3>
+// localhost:5173/reference/react : <h3>react 참고문서</h3>
+
+function App42(props) {
+  return <div></div>;
+}
+
+export default App42;
