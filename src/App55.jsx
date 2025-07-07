e209d724ddece9324256580de98111f986846ab4
sebaek <sebaek@gmail.com>
2025-07-07 15:46:04 +0900

    put, delete 요청

--- src/App55.jsx
+++ src/App55.jsx
@@ -14,9 +14,21 @@ function App55(props) {
   //       4번 클릭시 delete /api/main30/sub4 요청 보내기
 
   // spring : put /api/main30/sub3 받는 request handler method
+  function handleButton4Click() {
+    axios.delete("/api/main30/sub4");
+  }
+
+  function handleButton3Click() {
+    axios.put("/api/main30/sub3");
+  }
+
   //          delete /api/main30/sub4 받는 request handler method
   return (
     <div>
+      <button onClick={handleButton4Click}>4 delete</button>
+      <hr />
+      <button onClick={handleButton3Click}>3 put</button>
+      <hr />
       <button onClick={handleButton2Click}>2 delete 요청</button>
       <hr />
       <button onClick={handleButton1Click}>1 put 요청</button>
