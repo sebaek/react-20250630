a175107f3dd827592a99a2d72d428ec44877c82a
sebaek <sebaek@gmail.com>
2025-07-07 12:29:13 +0900

    post w/ json data

--- src/App54.jsx
+++ src/App54.jsx
@@ -14,8 +14,21 @@ function App54(props) {
     axios.post("/api/main29/sub3");
   }
 
+  function handleButton4Click() {
+    // 두 번째 파라미터는 json으로 직렬화(stringify, encoding)
+    axios.post("/api/main29/sub4", { name: "son", age: 44 });
+  }
+
+  function handleButton5Click() {
+    // 연습 : 적절한 코드 작성
+  }
+
   return (
     <div>
+      <button onClick={handleButton5Click}>post 5 w/ data</button>
+      <hr />
+      <button onClick={handleButton4Click}>post 4 w/ data</button>
+      <hr />
       <button onClick={handleButton3Click}>post 3</button>
       <hr />
       <button onClick={handleButton2Click}>post 2</button>
